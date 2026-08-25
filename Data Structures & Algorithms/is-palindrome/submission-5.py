03b5829ca6ae2e1482fe51007bbe2446850a1da3
Rhidita <36930197+Rhidz@users.noreply.github.com>
2026-08-25 14:57:26 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+        
+        while i < j:
+            
+            while i < j and not self.isAlphanumeric(s[i]):
+                i += 1
+            # if j> i and not self.isAlphanumeric(s[j]
+            while j> i and not self.isAlphanumeric(s[j]):
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False 
+            
+            i, j = i+1, j-1
+        
+        return True
+
+    def isAlphanumeric(self, c): 
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+
+    
+            
+            
+                
+                
